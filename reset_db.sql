afbdf6cfc750028185e3bfd0eb0f3d58c8cd0222
zfjohnson <124324322+zfjohnson@users.noreply.github.com>
2024-11-22 20:31:57 -0600

    Delete reset_db.sql

--- reset_db.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS trivia_game_trivia;
-DROP TABLE IF EXISTS trivia_game_movie_actors;
-DROP TABLE IF EXISTS trivia_game_movie;
-DROP TABLE IF EXISTS trivia_game_actor;
-DROP TABLE IF EXISTS trivia_game_director;
-DROP TABLE IF EXISTS trivia_game_studio;
-DROP TABLE IF EXISTS django_migrations;
