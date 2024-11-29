e3f9181b9a2a49780cfa171c61e02a333002f1b6
Zachary Johnson <z.f.johnson@outlook.com>
2024-11-29 00:00:15 -0600

    Updated DBconnector.py

--- DBconnector.py
+++ DBconnector.py
@@ -19,14 +19,15 @@ except mysql.connector.Error as err:
         print("Database does not exist")
     else:
         print(err)
-else:        
-    cursorObject = database.cursor()
-    if sys.argv[1]:
-        if sys.argv[1] == "create":
-            cursorObject.execute("CREATE DATABASE IF NOT EXISTS movie_mindread;")
-            print("Database created")
-        elif sys.argv[1] == "drop":
-            cursorObject.execute("DROP DATABASE IF EXISTS movie_mindread;")
-            print("Database dropped")
-    else:
-        print("Invalid argument. Usage: python DBconnector.py [create|drop]")
+else: 
+    try:       
+        cursorObject = database.cursor()
+        if sys.argv[1]:
+            if sys.argv[1] == "create":
+                cursorObject.execute("CREATE DATABASE IF NOT EXISTS movie_mindread;")
+                print("Database created")
+            elif sys.argv[1] == "drop":
+                cursorObject.execute("DROP DATABASE IF EXISTS movie_mindread;")
+                print("Database dropped")
+    except:
+        print("Usage: python DBconnector.py [create|drop]")
